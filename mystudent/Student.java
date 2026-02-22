d2cce7d7c49bee1a16dfdad372413fba892d9e81
Jasmine <jailiahc13@gmail.com>
2026-02-21 16:08:04 -0800

    Add files via upload

---
+++ mystudent/Student.java
@@ -0,0 +1,10 @@
+package mystudent;
+// student class
+public class Student {
+    String firstName;
+    String lastName;
+    double gpa;
+    String major;
+    int age;
+    boolean onProbation;
+}
